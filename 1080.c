478af9bc0ce2bab097a78586c5d89aa068344342
Miguel Matosinhos <miguelmatosinhos6@gmail.com>
2026-08-30 17:57:16 -0300

    Update 1080.c

--- 1080.c
+++ 1080.c
@@ -1,3 +1,15 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Algortimo e Estrutura de Dados 2026S1
+Nome        : Miguel Matosinhos Machado
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1080
+Data        : 30/08/2026
+Objetivo    : Encontrar o maior valor em um vetor e sua posição.
+Dificuldade : nenhum desafio maior
+Uso de IA   : não utilizei
+-------------------------------------------------------------------------- */
+
+
 #include <stdio.h>
 int main() {
     int vetor[100], i, maior = 0, posicao = 0;
